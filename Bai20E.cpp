8649de5c14deefed6d3cd20642f72da70de6e962
thuyduong7 <78083024+thuyduong7@users.noreply.github.com>
2021-02-03 14:04:26 +0700

    Create Bai20E.cpp

---
+++ Bai20E.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double incomeIncludeTax (double income, double tax){
+    income *= 0.91;
+    if (income > 2000000) tax = 500000 * 0.25 + (income - 2000000) * 0.20;
+    else if (income > 1500000) tax = 500000 * 0.1 + (income - 1500000) * 0.15;
+    else if (income > 1000000) tax = (income - 1000000) * 0.1;
+    return income - tax;
+}
+double incomeExcludeTax(double hour){
+    double income;
+    if (hour <= 100) income = 12000 * hour;
+    else {
+        double extraHour = hour - 100;
+        if (extraHour <= 50) income = 12000 * 100 + 16000 * extraHour;
+        else if (extraHour <= 100) income = 12000 * 100 + 16000 * 50 + 20000 * (extraHour - 50);
+        else income = 12000 * 100 + 16000 * 50 + 20000 * 50 + 25000 * (extraHour - 100);
+    }
+    return income;
+}
+double calculateLoan(double loan){
+    const double interestRate = 0.02;
+        loan = round(loan * (1 + interestRate));
+    return loan;
+}
+int main()
+{
+    double hour, income, tax = 0;
+    double loan = 10000000;
+    const double allowance = 1500000, livingCost = 2000000;
+
+
+    int i = 1;
+    while (loan > 0){
+        cout << "Thang thu " << i << endl;
+        cout << "So gio lam viec la: ";
+        cin >> hour;
+        income = incomeIncludeTax(incomeExcludeTax(hour),tax);
+        cout << fixed << setprecision(0) << "Thu nhap sau thue la: " << income << endl;
+        double pay = income + allowance - livingCost;
+        cout << "So tien lai ngan hang tinh vao cuoi thang la: " << calculateLoan(loan) - loan << endl;
+        loan = calculateLoan(loan) - pay;
+        if (loan > 0) cout << "So tien du no cua sinh vien vao dau thang la: " << loan << endl;
+        else {
+                cout << "Sau " << i << " thang sinh vien hoan tra het no" << endl;
+                cout << "Khoan thanh toan cuoi cung voi ngan hang la: " << loan + pay;
+        }
+        i++;
+    }
+    return 0;
+}
